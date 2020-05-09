3b7d7cfc6878f9e076a26102c705c315d5e175f1
xmdi <xmdi@protonmail.net>
2020-05-09 03:17:52 -0400

    added opengl

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # XGE
 xmdi game engine
 
-compile with `g++ -o test.exe src/* -IC:\SDL2\include\SDL2 -LC:\SDL2\lib -Bstatic -lmingw32 -lSDL2main -lSDL2` 
+compile with `g++ -o test.exe src/* -IC:\SDL2\include\SDL2 -LC:\SDL2\lib -lmingw32 -lSDL2main -lSDL2 -lopengl32` 
+
+run with `.\test.exe`
--- src/scratch.cpp
+++ src/scratch.cpp
@@ -1,46 +1,27 @@
 #include <stdio.h>
 #include <iostream>
 #include <SDL.h>
+#include <GL/gl.h>
 
-//Screen dimension constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
 int main( int argc, char* args[] )
 {
-	//The window we'll be rendering to
 	SDL_Window* window = NULL;
-	
-	//The surface contained by the window
-	SDL_Surface* screenSurface = NULL;
 
-	//Initialize SDL
-	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-	{
-		printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
-	}
-	else
-	{
-		//Create window
-		window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-		if( window == NULL )
-		{
-			printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
-		}
-		else
-		{
-			//Get window surface
-			screenSurface = SDL_GetWindowSurface( window );
+	window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_OPENGL );
+		
+	SDL_GLContext context = SDL_GL_CreateContext(window);
 
-			//Fill the surface white
-			SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
-			
-			//Update the surface
-			SDL_UpdateWindowSurface( window );
+	while (true) {
 
-			//Wait two seconds
-			SDL_Delay( 2000 );
-		}
+		glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+		glClearColor(1.f, 0.f, 0.f, 0.f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		SDL_GL_SwapWindow(window);
+
 	}
 
     std::cout<<"XGE"<<std::endl;
